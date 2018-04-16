dabb4bcb3d791683d5995b590afb14f116b9747d
Mark Cardamis <mark.cardamis@paydock.com>
2018-04-16 15:04:06 +1000

    Keep console open

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
         }
     }
 }
