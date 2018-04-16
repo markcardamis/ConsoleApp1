0401563942c0ae170ba57bede0b63447f45be34d
Mark Cardamis <mark.cardamis@paydock.com>
2018-04-16 20:43:28 +1000

    Added tests

---
+++ ConsoleApp1/StringLibrary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class StringLibrary
+    {
+        public static bool StartsWithUpper(this String str)
+        {
+            if (String.IsNullOrWhiteSpace(str))
+                return false;
+
+            Char ch = str[0];
+            return Char.IsUpper(ch);
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xunit;
+using ConsoleApp1;
+
+namespace UnitTest
+{
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestStartsWithUpper()
+        {
+            // Tests that we expect to return true.
+            string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.True(result,
+                       String.Format("Expected for '{0}': true; Actual: {1}",
+                                     word, result));
+            }
+        }
+
+        [Fact]
+        public void TestDoesNotStartWithUpper()
+        {
+            // Tests that we expect to return false.
+            string[] words = { "alphabet", "zebra", "abc", "αυτοκινητοβιομηχανία", "государство",
+                               "1234", ".", ";", " " };
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.False(result,
+                       String.Format("Expected for '{0}': false; Actual: {1}",
+                                     word, result));
+            }
+        }
+
+        [Fact]
+        public void DirectCallWithNullOrEmpty()
+        {
+            // Tests that we expect to return false.
+            string[] words = { string.Empty, null };
+            foreach (var word in words)
+            {
+                bool result = StringLibrary.StartsWithUpper(word);
+                Assert.False(result,
+                       String.Format("Expected for '{0}': false; Actual: {1}",
+                                     word == null ? "<null>" : word, result));
+            }
+        }
+    }
+}
